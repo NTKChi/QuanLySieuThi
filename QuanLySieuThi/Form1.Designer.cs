2e4d55173fd22b8e97cfec5f8a9d90b17c1b390d
iamhoanglong <80245882+iamhoanglong@users.noreply.github.com>
2022-04-27 21:08:55 +0700

    Add project files.

---
+++ QuanLySieuThi/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace QuanLySieuThi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTaikhoan = new System.Windows.Forms.Label();
+            this.lblMatkhau = new System.Windows.Forms.Label();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.txtTaikhoan = new System.Windows.Forms.TextBox();
+            this.chkHienthimatkhau = new System.Windows.Forms.CheckBox();
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTaikhoan
+            // 
+            this.lblTaikhoan.AutoSize = true;
+            this.lblTaikhoan.Location = new System.Drawing.Point(33, 61);
+            this.lblTaikhoan.Name = "lblTaikhoan";
+            this.lblTaikhoan.Size = new System.Drawing.Size(57, 15);
+            this.lblTaikhoan.TabIndex = 0;
+            this.lblTaikhoan.Text = "Tài khoản";
+            // 
+            // lblMatkhau
+            // 
+            this.lblMatkhau.AutoSize = true;
+            this.lblMatkhau.Location = new System.Drawing.Point(33, 127);
+            this.lblMatkhau.Name = "lblMatkhau";
+            this.lblMatkhau.Size = new System.Drawing.Size(57, 15);
+            this.lblMatkhau.TabIndex = 1;
+            this.lblMatkhau.Text = "Mật khẩu";
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(130, 119);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.Size = new System.Drawing.Size(151, 23);
+            this.txtMatkhau.TabIndex = 2;
+            // 
+            // txtTaikhoan
+            // 
+            this.txtTaikhoan.Location = new System.Drawing.Point(130, 61);
+            this.txtTaikhoan.Name = "txtTaikhoan";
+            this.txtTaikhoan.Size = new System.Drawing.Size(151, 23);
+            this.txtTaikhoan.TabIndex = 3;
+            // 
+            // chkHienthimatkhau
+            // 
+            this.chkHienthimatkhau.AutoSize = true;
+            this.chkHienthimatkhau.Location = new System.Drawing.Point(130, 159);
+            this.chkHienthimatkhau.Name = "chkHienthimatkhau";
+            this.chkHienthimatkhau.Size = new System.Drawing.Size(104, 19);
+            this.chkHienthimatkhau.TabIndex = 4;
+            this.chkHienthimatkhau.Text = "Hiện mật khẩu";
+            this.chkHienthimatkhau.UseVisualStyleBackColor = true;
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.Location = new System.Drawing.Point(57, 196);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangnhap.TabIndex = 5;
+            this.btnDangnhap.Text = "Đăng Nhập";
+            this.btnDangnhap.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(251, 196);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 287);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangnhap);
+            this.Controls.Add(this.chkHienthimatkhau);
+            this.Controls.Add(this.txtTaikhoan);
+            this.Controls.Add(this.txtMatkhau);
+            this.Controls.Add(this.lblMatkhau);
+            this.Controls.Add(this.lblTaikhoan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTaikhoan;
+        private System.Windows.Forms.Label lblMatkhau;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.TextBox txtTaikhoan;
+        private System.Windows.Forms.CheckBox chkHienthimatkhau;
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
